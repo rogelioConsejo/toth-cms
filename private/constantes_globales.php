c9b3790e3e1514f9f536fb6dafd2e751ec484eab
Rogelio <rogelio.consejo@gmail.com>
2019-04-30 15:42:18 -0500

    Limpieza de constantes globales

--- private/constantes_globales.php
+++ private/constantes_globales.php
@@ -3,6 +3,5 @@
 define("TEMPLATE_PATH", dirname(__FILE__));
 define("ROOT_PATH", substr(TEMPLATE_PATH, 0, strpos(TEMPLATE_PATH,'/assets')));
 define("PRIVATE_PATH", ROOT_PATH . '/private');
-define("PROJECT_PATH", dirname(PRIVATE_PATH));
-define("PUBLIC_PATH", PROJECT_PATH . '/public');
+define("PUBLIC_PATH", ROOT_PATH . '/public');
 define("ASSETS_PATH", ROOT_PATH . '/assets');
