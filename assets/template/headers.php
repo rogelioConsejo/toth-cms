eb0ade8e2ad489409be62668bf441fc171a03e34
Rogelio <rogelio.consejo@gmail.com>
2019-04-30 15:40:17 -0500

    Refactorización de templates a assets

--- assets/template/headers.php
+++ assets/template/headers.php
@@ -12,7 +12,7 @@ class Header_publico{
                       content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">
                 <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
                 <title>{$titulo}</title>
-                <link rel=\"stylesheet\" media=\"all\" href=\"" .ASSETS_PATH . "/estilo/toth-base.css\">
+                <link rel=\"stylesheet\" media=\"all\" href=\"" . ASSETS_PATH . "/estilo/toth-base.css\">
             </head>
             <body>
                 <header>
--- private/constantes_globales.php
+++ private/constantes_globales.php
@@ -1,8 +1,8 @@
 <?php
 
 define("TEMPLATE_PATH", dirname(__FILE__));
-define("ROOT_PATH", substr(TEMPLATE_PATH, 0, strpos(TEMPLATE_PATH,'/template')));
+define("ROOT_PATH", substr(TEMPLATE_PATH, 0, strpos(TEMPLATE_PATH,'/assets')));
 define("PRIVATE_PATH", ROOT_PATH . '/private');
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH . '/public');
-define("ASSETS_PATH", PRIVATE_PATH . '/assets');
+define("ASSETS_PATH", ROOT_PATH . '/assets');
