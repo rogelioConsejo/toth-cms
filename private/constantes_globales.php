3164b980ac7f667561d591ca70248531c4346fb9
Rogelio <rogelio.consejo@gmail.com>
2019-05-03 13:30:05 -0500

    Generalizacion de paths globales
    Para que funcione en diferentes entornos (desarrollo, despliegue, etc.)

--- private/constantes_globales.php
+++ private/constantes_globales.php
@@ -1,5 +1,6 @@
 <?php
 
-define("PRIVATE_PATH", dirname(__FILE__));
+#define("PRIVATE_PATH", dirname(__FILE__));
+define("PRIVATE_PATH", $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
 define("ROOT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", ROOT_PATH . '/public');
